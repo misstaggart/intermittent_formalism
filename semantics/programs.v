74054b1fc067c0caad80f4174644269cb58c9874
Elliot Spargo <nspargo@andrew.cmu.edu>
2020-06-07 14:46:44 -0400

    Completely new trace type with pointers; going to attempt proofs now

--- semantics/programs.v
+++ semantics/programs.v
@@ -6,9 +6,11 @@ From TLC Require Import LibTactics LibLogic.
 From mathcomp Require Import ssreflect ssrfun ssrbool eqtype.
 From Semantics Require Import algorithms.
 
-Open Scope list_scope.
 
 Open Scope type_scope.
+
+
+Open Scope list_scope.
 (************* program traces*****************)
 
 (*trace helpers*)
@@ -52,17 +54,42 @@ Notation step :=(context * obseq * context).
  Check classicT.
 
  (*Michael look here*)
- Program Definition getstart (L: list step) (H: L <> []):=
+ Program Definition gethead (L: list context) (H: L <> []):=
    match L with
      [] => !
-    | (out, _, _)::xs => out end.
+    | out::xs => out end.
+ (*trying with pointers*)
+ Inductive step_term : (list context) -> (list obseq) -> (list the_write_stuff) -> Prop :=
+   Step_Single : forall{C1: context} {N2: nvmem} {V2: vmem} {O: obseq} {W: the_write_stuff},
+     cceval_w C1 O (N2, V2, (Ins skip)) W -> step_term (C1::[(N2, V2, (Ins skip))]) [O] [W]
+ | Step_Many: forall{C1 C2: context} {O1: obseq} {W1: the_write_stuff} (*makes program bigger*)
+               {Crest: (list context)} {Orest: (list obseq)} {Wrest: (list the_write_stuff)}    (H: Crest <> nil), 
+     step_term Crest Orest Wrest ->
+     cceval_w C1 O1 (gethead Crest H) W1 ->
+     step_term (C1::Crest) (O1:: Orest) (W1:: Wrest).
+
+ Inductive trace_c (CL: list context) (WL: list the_write_stuff) (OL: list obseq): nat -> nat -> Prop :=
+   Trace_C: forall
+             (s e: nat)
+             (H1: s < (List.length CL))
+             (H2: e < (List.length CL)),
+             step_term CL OL WL ->
+             trace_c CL WL OL s e.
+Theorem trace_append : forall {CL: list context} {WL: list the_write_stuff} {OL: list obseq} {s1 mid e2: nat}
+                                          (T1: trace_c CL WL OL s1 mid) (T2: trace_c CL WL OL mid e2),
+     trace_c CL WL OL s1 e2.
+  intros. inversion T1. inversion T2. subst.
+  apply (Trace_C CL WL OL s1 e2 H1 H5 H6). Qed.
+
+
+ 
 
  (*contexts at end is ending context*)
  (*can take anything out, ie, step the front, and the end stays the same*)
  (*want it to be so that combining the stuff is just appending
   but first write doesn't work like that*)
  (*why is it that I can't combine the write data in real time again?*)
- (*here you're going up AND down...won't work*)
+ (*here you're going up AND down...won't work
  Inductive trace_c: (list step) -> context -> (list the_write_stuff) -> Type :=
   CTrace_Empty: forall(C: context), trace_c nil C nil
 | CTrace_single: forall {C1 C2: context} {O: obseq} {W: the_write_stuff},
@@ -77,7 +104,7 @@ Notation step :=(context * obseq * context).
     trace_c L Cmid WT ->
     L <> [] ->
     cceval_w Cmid O3 C3 W3 ->
-    trace_c (L ++ [(Cmid, O3, C3)]) C3 (WT ++ [W3]). (*steps the back of the trace*)
+    trace_c (L ++ [(Cmid, O3, C3)]) C3 (WT ++ [W3]). (*steps the back of the trace*)*)
 (*maybe have append instead of Step2 as a constructor*)
  (*Theorem trace_step: forall{L: list step} {Cend: context} {WL: list the_write_stuff},
                       trace_c L Cend WL ->
@@ -90,7 +117,6 @@ Notation step :=(context * obseq * context).
      - simpl. constructor.
      - simpl.  rewrite H in T.*)
 
- Theorem trace_app: forall{L1 L2: list step} {Cend: context} {WT1 WT2: list the_write_stuff}
 
 
 (*intermittent traces*)
@@ -119,23 +145,37 @@ Definition multi_step_c (C1 C2: context) (O: obseq) :=
     [] => emptysets
   |(((_, _, _), W1):: xs) => append_write W1 (acc_wts xs) end.*)
 
-Fixpoint acc_obs (L: list step) :=
+Fixpoint acc_obs (L: list obseq) :=
   match L with
     [] => []
-  |((_, obs, _):: xs) => obs ++ (acc_obs xs) end.
+  |(obs:: xs) => obs ++ (acc_obs xs) end.
 
-Definition Wt {C1: context} {L: list step} {WL: list the_write_stuff} (T: trace_c L C1 WL) :=
-getwt (combine_write WL).
+(*(nth_error CL e) *)
 
-Definition FstWt {C1: context} {L: list step} {WL: list the_write_stuff} (T: trace_c L C1 WL)
-            := getfstwt (combine_write WL).
-Definition getobs {C1: context} {L: list step} {WL: list the_write_stuff} (T: trace_c L C1 WL) :=
-  acc_obs L.
+Definition Wt {CL: list context} {WL: list the_write_stuff} {OL: list obseq} {s e: nat} (T: trace_c CL WL OL s e) :=
+getwt (combine_write (firstn (e- s + 1) (skipn s WL))).
 
+
+Definition FstWt {CL: list context} {WL: list the_write_stuff} {OL: list obseq}{s e: nat} (T: trace_c CL WL OL s e) :=
+getfstwt (combine_write (firstn (e- s + 1) (skipn s WL))).
+
+
+Definition getobs {CL: list context} {WL: list the_write_stuff} {OL: list obseq}{s e: nat}
+           (T: trace_c CL WL OL s e) :=
+  acc_obs ( (firstn (e- s + 1) (skipn s OL))).
+
+Definition getstart {CL: list context} {WL: list the_write_stuff} {OL: list obseq} {s e: nat}
+           (T: trace_c CL WL OL s e) :=
+  nth_error CL s.
+
+Definition getend {CL: list context} {WL: list the_write_stuff} {OL: list obseq} {s e: nat}
+           (T: trace_c CL WL OL s e) :=
+  nth_error CL e.
 (**********************************************************************************)
 
-Definition multi_step_c (C1: context) (O: obseq)  :=
-  exists (L: list step) (W: list the_write_stuff) (T: trace_c L C1 W), (getobs T) = O.
+Definition multi_step_c (C1 C2: context) (O: obseq)  :=
+  exists {CL: list context} {WL: list the_write_stuff} {OL: list obseq}
+    {s e: nat} (T: trace_c CL WL OL s e), (getobs T) = O /\ (getstart T) = (Some C1) /\ (getend T) = (Some C2).
 
 (*Definition multi_step_i (C1 C2: iconf) (O: obseq) :=
     exists W: the_write_stuff, trace_i C1 C2 O W.*)
@@ -162,16 +202,22 @@ same_mem: forall {N0 N1 N2 Ncomp: nvmem}
                   {V0 V1 V2: vmem}
                   {c0 c1 c2: command}
                   {w: warvars}
-                  {L1 L2: list step}
-                  {W1 W2: list the_write_stuff}
-                  (T1: trace_c L1 (N0, V0, c0) (N1, V1, c1) W1 )
-                  (T2: trace_c L2 (N1, V1, c1) (N2, V2, (incheckpoint w);; c2) W2), (*c2 could be skip*)      (getdomain N1) = (getdomain Ncomp) 
+                  {L: list context}
+                  {O: list obseq}
+                  {W: list the_write_stuff}
+                  {s1 mid e2: nat}
+                  (T1: trace_c L W O s1 mid)
+                  (T2: trace_c L W O mid e2),
+                  (getstart T1 = Some (N0, V0, c0)) ->
+                  (getend T1 = Some (N1, V1, c1)) ->
+                  (getend T2 = Some (N2, V2, (incheckpoint w);; c2)) -> 
+                  (getdomain N1) = (getdomain Ncomp)
                   -> not (In checkpoint (getobs T1))
                   -> not (In checkpoint (getobs T2)) (*checks checkpoint T2 ends on is nearest checkpoint*)
                  -> (forall(l: loc),
                       not((getmap N1) l = (getmap Ncomp) l)
-                      -> ((In l (Wt T2)) /\ (In l (FstWt (CTrace_App T1 T2 (refl_context (N1, V1, c1))
-                                                          ))) /\ not (In l (Wt T1))))
+                      -> ((In l (Wt T2)) /\ (In l (FstWt (trace_append T1 T2)
+                                                          ))) /\ not (In l (Wt T1)))
                   -> same_pt N0 V0 c0 c1 N1 Ncomp.
 (*Definition 5*)
 (*N0, V0 is starting state for both executions
@@ -184,9 +230,13 @@ valid_mem: forall {N N0 N1 N2: nvmem}
                   {V0 V1 V2: vmem}
                   {c c1: command}
                   {w: warvars}
-                  {L: list step}
+                  {CL: list context}
+                  {O: list obseq}
                   {W: list the_write_stuff}
-                  (T: trace_c L (N1, V1, c) (N2, V2, (incheckpoint w) ;; c1 ) W),
+                  {s e: nat}
+                  (T: trace_c CL W O s e),
+    (getstart T) = Some (N1, V1, c) ->
+    (getend T) = Some (N2, V2, (incheckpoint w) ;; c1 ) ->
                   (getdomain N1) = (getdomain N0) 
                   -> not (In checkpoint (getobs T)) (*checks checkpoint T ends on is nearest checkpoint*)
                  -> (forall(l: loc),
