370bc410e0db43cbebbd1df543a158f4fb1fd141
Elliot Spargo <nspargo@andrew.cmu.edu>
2021-01-19 13:20:27 -0500

    transferred DINO proofs

---
+++ semantics/proofs_d.v
@@ -0,0 +1,49 @@
+Set Warnings "-notation-overridden,-parsing".
+From Coq Require Import Bool.Bool Init.Nat Arith.Arith Arith.EqNat
+     Init.Datatypes Strings.String Program Logic.FunctionalExtensionality.
+Require Export Coq.Strings.String.
+From mathcomp Require Import ssreflect ssrfun ssrbool eqtype seq fintype ssrnat ssrfun path.
+From TLC Require Import LibTactics LibLogic.
+From Semantics Require Export programs semantics algorithms lemmas_1
+     lemmas_0 proofs_0. (*shouldn't have to import both of these*)
+
+Lemma onePointone: forall(N N' W W' R R': warvars) (l: instruction),
+    DINO_ins N W R l N' W' R' -> subseq N N'.
+Proof. intros. induction H; try((try apply subseq_tl); apply (subseq_refl N)).
+       apply (subseq_cons N (inl x)).
+       apply suffix_subseq.
+Qed.
+
+
+Lemma onePointtwo: forall(N N' W R: warvars) (c c': command),
+    DINO N W R c c' N' -> subseq N N'.
+Proof. intros. induction H; try(apply onePointone in H); try apply (incl_refl N); try assumption.
+       -  apply (subseq_trans H IHDINO).
+       - apply subseq_app_rr. assumption. apply subseq_refl.
+ Qed.
+
+Lemma Two: forall(N N' W W' R R' N1: warvars) (l: instruction),
+    DINO_ins N W R l N' W' R' -> subseq N' N1 -> WAR_ins N1 W R l W' R'.
+  intros. induction H; try ((constructor; assumption) || (apply War_noRd; assumption)).
+            (apply WAR_Checkpointed);
+              (repeat assumption).
+            move / in_subseq : H0.
+            intros.
+            apply (H0 (inl x) (mem_head (inl x) N)).
+            apply WAR_Checkpointed_Arr; try assumption. apply (subseq_app_l H0).
+
+Qed.
+
+
+Theorem DINO_WAR_correct: forall(N W R N': warvars) (c c': command),
+    DINO N W R c c' N' -> (forall(N1: warvars), subseq N' N1 -> WARok N1 W R c').
+  intros N W R c c' N' H. induction H; intros N0 Hincl.
+  - eapply WAR_I. applys Two H Hincl.
+  - eapply WAR_Seq. applys Two H. apply onePointtwo in H0.
+    apply (subseq_trans H0 Hincl).
+    apply (IHDINO N0 Hincl).
+  - eapply WAR_If; (try eassumption);
+      ((apply IHDINO1; apply subseq_app_l in Hincl)
+       || (apply IHDINO2; apply subseq_app_r in Hincl)); assumption.
+  - intros. apply WAR_CP. apply IHDINO. apply (subseq_refl N').
+Qed.
